d2ca475407cc4901797567208cc8934912e70739
Donovan <donosaurusjk@gmail.com>
2025-11-20 11:37:48 -0500

    added randomly generated cards

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function imageassign(){
-    const cards = document.getElementsByClassName("card");
+function imageAssign () {
+    const ids = ['cat', 'joker', 'ghostface', 'smoke', 'ghost', 'house', 'pumpkins', 'pennywise', 'reaper', 'tree'];
+    const cards = Array.from(document.getElementsByClassName("card"));
+    console.log(cards);
+    for (let idInd=0; idInd<ids.length; idInd++) {
+        for (let i=0; i<2; i++) {
+            let randCardInd = Math.floor(Math.random() * cards.length);
+            const card = cards.splice(randCardInd, 1)[0];
+            card.id=ids[idInd];
+        }
+    }
 }
